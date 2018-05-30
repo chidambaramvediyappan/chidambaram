4764fe0314e029c3130a5d4d802c9adce5dc8afa
chidambaramvediyappan <vchidambaram25@gmail.com>
2018-05-30 18:08:16 +0530

    REFRENCE

---
+++ REFRENCE.java
@@ -0,0 +1,17 @@
+package edition;
+
+public class REFRENCE {
+	public static void main(String[] args)
+	{
+		//String true;
+		int number =50;
+		number++;
+		number--;
+		int negnumber=-50;
+		boolean isfalse = true ;
+		
+		System.out.println(!isfalse);
+		
+	}
+
+}
