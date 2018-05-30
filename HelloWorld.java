c7df9eac92e416b72d9c0676da5e411829e9e5c4
chidambaramvediyappan <vchidambaram25@gmail.com>
2018-05-30 16:23:30 +0530

    simple

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,9 @@ public class HelloWorld
 public static void main(String s[])
 {
 System.out.println("wellcome to java Trianing!!!");
+byte one =100;
+byte two =20;
+int three =one+two;
+System.out.println(three);
 }
 }
