31aecd876187221107828c1d81be02f42a87c9c1
chidambaramvediyappan <vchidambaram25@gmail.com>
2018-05-30 18:30:22 +0530

    more operator

---
+++ javabasic/src/com/javabasic/good.java
@@ -0,0 +1,21 @@
+package com.javabasic;
+
+public class good {
+	public static void main(String[] args)
+	{
+	
+	int count =10;
+	count++;
+	count--;
+	int negcount =-60;
+	boolean isfalse=true;
+	System.out.println(!isfalse);
+	System.out.println(count < 60);
+	System.out.println(count > 60);
+	System.out.println(count < 10);
+	System.out.println(count > 10);
+	
+	
+
+}
+}
