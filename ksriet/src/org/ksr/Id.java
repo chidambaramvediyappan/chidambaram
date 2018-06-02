f3f6602b27e1273537dd3742c3bffc0a8386faf8
chidambaramvediyappan <vchidambaram25@gmail.com>
2018-06-02 15:57:30 +0530

    commit

---
+++ ksriet/src/org/ksr/Id.java
@@ -0,0 +1,18 @@
+package org.ksr;
+
+import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
+
+public class Id {
+public static void main(String[] args) {
+	
+	
+Student  e1=new Student();
+e1.setFirstName("basker");
+	e1.setLastName("v");System.err.println(e1.toString());
+	
+	Student e2 =  new Student;
+	e2.set
+}     
+
+		}
+
